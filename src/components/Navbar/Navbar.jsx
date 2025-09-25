357760070b0d1273a491c9c54b8c6f3e7bc443ed
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-09-25 12:28:46 +0530

    modify app.jsx

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return 
+    <div>
+        {/* upper Navbar*/}
+    <div>
+        <div>
+            <div>
+                <a herf="#">
+                    <img src={Logo} alt="Logo"></img>
+                </a>
+
+            </div>
+        </div>
+    </div>
+    </div>
+    {/* lower Navbar*/}
+<div>
+    </div>
+  
+}
+
+export default Navbar
