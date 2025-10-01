2931d83ee147913f5bf0502a9f01d34060604c4e
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-10-01 22:00:03 +0530

    rated product page added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,6 @@ import Image3 from "../../assets/hero/sale.png";
 import Slider from "react-slick";
 
 const ImageList = [
- 
   {
     id: 1,
     img: Image1,
@@ -13,7 +12,7 @@ const ImageList = [
     description:
       "Shop the latest trends in women's fashion with our exclusive 30% discount. From dresses to accessories, find your perfect style today!",
   },
-   {
+  {
     id: 2,
     img: Image2,
     title: "Upto 50% off on all Men's wear",
@@ -30,8 +29,7 @@ const ImageList = [
 ];
 
 const Hero = () => {
-
-     var settings = {
+  var settings = {
     dots: false,
     arrows: false,
     infinite: true,
@@ -44,9 +42,6 @@ const Hero = () => {
     pauseOnFocus: true,
   };
 
-
-  
-
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
       {/* background pattern */}
@@ -54,12 +49,12 @@ const Hero = () => {
       {/* hero section */}
       <div className="container pb-8 sm:pb-0">
         <Slider {...settings}>
-            {ImageList.map((data) => (
-          <div>
-            <div className="grid grid-cols-1 sm:grid-cols-2">
-              {/* text content section */}
-              <div className="flex flex-col justify-center order-2 gap-4 pt-4 text-center sm:pt-0 sm:text-left sm:order-1">
-               <h1
+          {ImageList.map((data) => (
+            <div>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text content section */}
+                <div className="flex flex-col justify-center order-2 gap-4 pt-4 text-center sm:pt-0 sm:text-left sm:order-1">
+                  <h1
                     data-aos="zoom-out"
                     data-aos-duration="500"
                     data-aos-once="true"
@@ -80,24 +75,24 @@ const Hero = () => {
                     data-aos-duration="500"
                     data-aos-delay="300"
                   ></div>
-                <button className="px-4 py-2 text-white duration-200 rounded-full bg-gradient-to-r from-primary to-secondary hover:scale-105">
-                  Order Now
-                </button>
-              </div>
+                  <button className="px-4 py-2 text-white duration-200 rounded-full bg-gradient-to-r from-primary to-secondary hover:scale-105">
+                    Order Now
+                  </button>
+                </div>
 
-              {/* image section */}
-              <div className="order-1 sm:order-2">
-                <div className="relative z-10">
-                <img
-                  src={data.img}
-                  alt="Hero"
-                   className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto"
-                />
+                {/* image section */}
+                <div className="order-1 sm:order-2">
+                  <div className="relative z-10">
+                    <img
+                      src={data.img}
+                      alt="Hero"
+                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-            ))}
+          ))}
         </Slider>
       </div>
     </div>
