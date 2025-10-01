2d836c4c498c1fa63b7d2e2743ee51a9452c0cd8
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-10-01 22:03:12 +0530

    update TopProduct component

--- src/components/TopProducts/TopProduct.jsx
+++ src/components/TopProducts/TopProduct.jsx
@@ -9,13 +9,13 @@ const TopProduct = () => {
       {/* Header section */}
       <div className="text-center mb-10 max-w-[600px] mx-auto">
         <p data-aos="fade-up" className="text-sm text-primary">
-          Top Selling Products For You
+          Top Rated Products For You
         </p>
         <h1 data-aos="fade-up" className="text-3xl font-bold">
             Products
             </h1>
             <p data-aos="fade-up" className="text-sm text-gray-400">
-              Discover our top-selling products that customers love.
+              Discover our top-rated products
             </p>
           </div>
     </div>
